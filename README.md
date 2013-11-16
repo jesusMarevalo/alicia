2494e53c1961ca6adff121f91dd494bf786b15e2
jesusMarevalo <jesus.m.arevalo@gmail.com>
2013-11-16 12:45:43 +0100

    creating gh-pages branch

---
+++ README.md
@@ -0,0 +1,17 @@
+alicia
+======
+alicia website static
+
+git commit -am "message"
+git push origin master
+
+to update gh-pages
+==================
+
+git checkout gh-pages  // go to the gh-pages branch
+git rebase master      // bring gh-pages up to date with master
+git push origin gh-pages   // commit the changes
+git checkout master    // return to the master brach
+
+ 
+
---
+++ js/funciones.js
@@ -0,0 +1,56 @@
+function mostrarCaja(x){
+	$('#box'+x+' .caja_negra').css({'opacity':'0.8'});
+	$('#box'+x+' .subtitulo').css({'opacity':'1'});
+	$('#box'+x+' .link').css({'opacity':'1'});
+}
+
+function esconderCaja(x){
+	$('#box'+x+' .caja_negra').css({'opacity':'0'});
+	$('#box'+x+' .subtitulo').css({'opacity':'0'});
+	$('#box'+x+' .link').css({'opacity':'0'});
+}
+
+
+function abrir(x){
+	$('#descripcion'+x).toggle();
+}
+
+function ajustarImagenes(){
+	var w = $('#content').width();
+	
+	var big = w;
+	
+	var medium = (w/2);
+	var small = (w/4);
+	var triple = (w/4*3);
+	$('.big img').css({'width':big});
+	$('.medium img').css({'width':medium});
+	$('.small img').css({'width':small});
+	$('.triple img').css({'width':triple});
+
+}
+
+jQuery(window).load(function(){
+
+	ajustarImagenes();
+
+});
+
+$(document).ready(function(){ 
+	ajustarImagenes();
+ 
+});
+
+$(window).bind('resize', function(e){
+	window.resizeEvt;
+	$(window).resize(function() {
+		clearTimeout(window.resizeEvt);
+		window.resizeEvt = setTimeout(function(){
+		}, 250);
+	});
+});
+
+$(window).resize(function(){ 
+	ajustarImagenes();
+
+});
